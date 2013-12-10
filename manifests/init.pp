154aa9193a30108788a5f53c05091fa038c903ab
Leonardo Thibes <eu@leonardothibes.com>
2013-12-10 11:37:30 -0200

    implementando o manifest do modulo

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,22 @@
-# == Class: vim
-#
-# Full description of class vim here.
-#
-# === Parameters
-#
-# Document parameters here.
-#
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if it
-#   has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should not be used in preference to class parameters  as of
-#   Puppet 2.6.)
-#
-# === Examples
-#
-#  class { vim:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ]
-#  }
-#
-# === Authors
-#
-# Author Name <author@domain.com>
-#
-# === Copyright
-#
-# Copyright 2011 Your name here, unless otherwise noted.
-#
 class vim {
 
+	$vim     = "vim"
+	$vimpath = "/etc/vim"
 
+	package {[ "$vim" , "$vim-puppet" ]: ensure => present}
+	exec {"vim-addons install puppet": path => "/usr/bin"}
+	exec {"update-alternatives --set editor /usr/bin/vim.basic":
+		path    => "/usr/bin:/usr/sbin:/bin",
+		unless  => "test /etc/alternatives/editor -ef /usr/bin/vim.basic",
+		require => Package["$vim"],
+	}
+
+	file {"$vimpath/vimrc.local":
+		ensure  => present,
+		source  => "puppet:///modules/vim/vimrc.local",
+		owner   => root,
+		group   => root,
+		mode    => 0644,
+		require => Package["$vim"],
+	}
 }
